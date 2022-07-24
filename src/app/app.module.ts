2aea67a155fd44fee9d9877244ec5032e2dd8550
Eslam-Mohamed-Bekhit <islam.m.bekhit@gmail.com>
2022-07-25 00:07:46 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './components/product-list/product-list/product-list.component';
+import { ProductItemComponent } from './components/product-item/product-item/product-item.component';
+import { CartComponent } from './components/cart/cart/cart.component';
+import { ConfirmationComponent } from './components/confirmation/confirmation/confirmation.component';
+import { ProductItemDetailComponent } from './components/product-item-detail/product-item-detail/product-item-detail.component';
+import { HeaderComponent } from './layout/header/header/header.component';
+import { NotFoundComponent } from './components/not-found/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductItemComponent,
+    CartComponent,
+    ConfirmationComponent,
+    ProductItemDetailComponent,
+    HeaderComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
